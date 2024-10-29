3044570eb4af2008010491b1577e2e20069c1b10
Pablo Sierra Lorente <pablosierralorente@outlook.es>
2024-10-29 10:40:56 +0100

    mainframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
-import { ArtistGallery } from './components/ArtistGallery'
 import { MainFrame } from './components/MainFrame'
 
 function App() {
 
   return (
     <>
-    <MainFrame/>
-    <ArtistGallery/>
+    <body>
+      <header></header>
+      <MainFrame/>
+      <footer></footer>
+    </body>
     </>
   )
 }
--- src/components/MainFrame.jsx
+++ src/components/MainFrame.jsx
@@ -1,10 +1,14 @@
-/* eslint-disable react/prop-types */
-export function MainFrame({children}) {
+import imagenPortada from '../assets/DsC02074.jpg'
+
+export function MainFrame() {
     return(
         <>
-        <section>
-            <div><h1>{children}</h1></div>
-            <div><h2>{children}</h2></div>
+        <section className='portada'>
+            <img className="portada-img" src={imagenPortada} alt="imagen portada" />
+            <div className='portadaText'>
+                <h1 className='portada-titulo'>Rafael Moreno</h1>
+                <h2 className='portada-subtitulo'>Productor musical</h2>
+            </div>
         </section>
         </>
     )
