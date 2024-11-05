7153c934a3a7b8e71bc2765402e5c851b3bf3b7d
Pablo Sierra Lorente <pablosierralorente@outlook.es>
2024-11-05 13:45:56 +0100

    update strucutre

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import './App.css'
 import { ArtistGallery } from './components/ArtistGallery'
+import { ContactForm, ContactInfo } from './components/Contact'
 import { MainFrame } from './components/MainFrame'
 import { Replayer } from './components/Replayer'
 
 function App() {
 
   return (
-    <>
-    <body>
+    <div className='App'>
       <header></header>
       <MainFrame/>
       <Replayer/>
       <ArtistGallery/>
+      <section className='contact'>
+        <ContactForm/>
+      <div className='contact-info'>
+        <ContactInfo/>
+      </div>
+      </section>
       <footer></footer>
-    </body>
-    </>
+    </div>
   )
 }
 
